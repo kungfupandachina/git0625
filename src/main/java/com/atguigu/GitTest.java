315f7c7737c3af5b8213b94058feca1ed7404a5d
kungfupandachina <kungfupandachina@126.com>
2021-10-29 21:25:23 +0800

    third commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("get Test");
         System.out.println("get Test2");
+        System.out.println("get Test3");
     }
 
 }
